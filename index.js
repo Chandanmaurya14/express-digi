4aa88dd9531acceac774dfa3f07daf46008a9e21
Chandan Maurya <chandanmaurya14k@gmail.com>
2025-02-06 01:05:11 +0530

    Add loggger file and more

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ import "dotenv/config";
 
 import express from "express";
 
+import logger from "./loggger.js";
+import morgan from "morgan";
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
 
 app.post("/teas", (req, res) => {
+  logger.info("A post request is made to add a new tea");
   const { name, price } = req.body;
   const newTea = { id: nextId++, name, price };
   teaData.push(newTea);
@@ -42,7 +62,7 @@ app.put("/teas/:id", (req, res) => {
   const { name, price } = req.body;
   tea.name = name;
   tea.price = price;
-  res.send(200).send(tea);
+  res.status(200).send(tea);
 });
 
 // default route
